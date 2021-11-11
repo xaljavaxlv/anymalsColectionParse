280019f7244bcd40c48576caf8c20994ccb48dfd
xlv <xaljavamail@gmail.com>
2021-11-11 23:35:55 +0800

    Initial Commit

---
+++ anymalsColectionParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  anymalsColectionParse
+//
+//  Created by Vlad Zavada on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
